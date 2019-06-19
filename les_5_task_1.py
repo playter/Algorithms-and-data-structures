855925392b9fc481908abf84d65a0aea45a5d62f
playter <play.ts@yandex.ru>
2019-06-19 19:46:49 +0300

    убрал коментарии

--- les_5_task_1.py
+++ les_5_task_1.py
@@ -40,19 +40,3 @@ for i in enterprises:
         n_sr.append(list(i))
 print(f'Предприятия, чья прибыль ниже среднего: {n_sr}')
 print(f'Предприятия, чья прибыль выше среднего: {v_sr}')
-
-
-# studs = {}
-# n = int(input( "Количество студентов: " ))
-# s = 0
-# for i in range(n):
-#     sname = input(str(i + 1) + "-й студент: ")
-#     point = int(input("Балл: "))
-#     studs[sname] = point
-#     s += point
-#
-# avrg = s / n
-# print( "\nСредний балл: %.0f. Студенты с баллом выше среднего:" % avrg)
-# for i in studs:
-#     if studs[i] > avrg:
-#         print(i)
